4cf44053b7565c4fee1d2a305bab1dacfb99c406
KyrraRose <krbosland1@gmail.com>
2025-10-03 14:57:38 -0400

    The library is open! We're currently accepting donations for more books.

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -35,7 +35,7 @@ public class NeighborhoodLibrary {
 
         //Library home screen! I found some text stars to add a bit of UX to our UI and delight readers.
         while (true) {
-            System.out.print("˚　　✦　　　.　　.  ˚　.　　 . ✦　　˚　　 . ★⋆.　　　.　˚\n  ==== Welcome to the Neighborhood Library! ====\n          What would you like to do?\n          [A] Show Available Books\n          [B] Show Checked Out Books\n          [X] Exit the Application\n*　　✦　　.　　.　　　✦　˚ 　　˚　.˚　　　.　　. ˚　.　　 ✦\nType Here: ");
+            System.out.print("˚　　✦　　　.　　.  ˚　.　　 . ✦　　˚　　 . ★⋆.　　　.　˚\n  == Welcome to the Galactic Neighborhood Library! ==\n          What would you like to do?\n          [A] Show Available Books\n          [B] Show Checked Out Books\n          [X] Exit the Application\n*　　✦　　.　　.　　　✦　˚ 　　˚　.˚　　　.　　. ˚　.　　 ✦\nType Here: ");
             String choice = scanner.nextLine().trim().toLowerCase();
 
             switch (choice) {
@@ -47,42 +47,57 @@ public class NeighborhoodLibrary {
                     while (subMenu){
                         switch (choiceCheck){
                             case "y":
-                                System.out.println("What is your full name?. ✦");
+                                System.out.print("What is your full name?. ✦\nType Here: ");
                                 String userName = scanner.nextLine().trim();
                                 checkOutBook(libraryInventory,userName,scanner);
-
+                                subMenu = false;
                                 break;
+
                             case "n":
                                 subMenu = false;
                                 break;
                             default:
                                 System.out.println("Please enter either Y or N!");
                         }
-                        if (choiceCheck.equals("y")){
-                            System.out.println("Would you like to check out another book?˚　.˚\n    [Y] Yes\n    [N] No\nType here: ");
-                        }else{
-                            System.out.println("Here are the books you have checked out:");
-                        }
 
                     }
-                    // note for lunch key, now do check out prompts
 
                     break;
 
                 case "b":
+                    boolean subMenu2 = true;
+                    showCheckedOut(libraryInventory);
+                    System.out.print("Would you like to check in a book? . ★⋆.\n    [Y] Yes\n    [N] No\nType here: ");
+                    String choiceCheck2 = scanner.nextLine().trim().toLowerCase();
+                    while (subMenu2){
+                        switch (choiceCheck2){
+                            case "y":
+                                checkInBook(libraryInventory,scanner);
+
+                                break;
+
+                            case "n":
+                                subMenu2 = false;
+                                break;
+                            default:
+                                System.out.println("Please enter either Y or N!");
+                        }
+
+                    }
+
 
                     break;
                 case "x":
-                    System.out.println("˚　.　　 . ✦　　˚　　 . ★⋆.　　　.\nClosing! Have a Space-Tastic Day!\n✦　　.　　.　　　✦　˚ 　　˚　.");
+                    System.out.println("˚　.　　 . ✦　　˚　　 . ★⋆.　　　.\nClosing! Have a Space-Tastic Time Reading!\n✦　　.　　.　　　✦　˚ 　　˚　.");
                     System.exit(0);
                     scanner.close();
                     break;
                 default:
-                    System.out.println("Anomaly spotted! Please pick a valid choice from the menu.");
+                    System.out.println("✦ Anomaly spotted! Please pick a valid choice from the menu. ✦");
 
 
             }
-            System.out.println("Press [ENTER] to Continue!. ★⋆.");
+            System.out.println("\nPress [ENTER] to Continue!. ★⋆.");
             scanner.nextLine();
 
         }
@@ -92,34 +107,85 @@ public class NeighborhoodLibrary {
     }
     public static void showAvailable(Book[] libraryInventory){
         int i = 1;
+        System.out.println("\nAvailable Books ˚　.　　 . ✦　　˚　　 . ★⋆.　　　.✦　　.　　.　　　✦　˚ 　　˚　. \n");
         for (Book book:libraryInventory){
             if (book.isCheckedOut()==false){
-                System.out.printf("Book %d: %d %s %s\n",i,book.getId(),book.getIsbn(),book.getTitle(),book.getAuthor());
+                System.out.printf("Book %d: %d %s by %s ISBN: %s \n\n",i,book.getId(),book.getTitle(),book.getAuthor(),book.getIsbn());
                 i++;
             }
         }
 
     }
-    public static Book[] checkOutBook(Book[] libraryInventory,String userName,Scanner scanner){
+    public static void checkOutBook(Book[] libraryInventory,String userName,Scanner scanner){
         Book[] cart = new Book[libraryInventory.length]; //you COULD check them all out if you wanted to, I guess
-        System.out.println("What is the title of the book?");
-        String bookTitle = scanner.nextLine();
+        boolean choice = true;
         int i = 0; //counter
-        for (Book book:libraryInventory){
-            if (book.getTitle().equalsIgnoreCase(bookTitle)){
-                book.setCheckedOutTo(userName);
-                book.setCheckedOut(true);
-                cart[i]= book;
+        while (choice){
+            if (i == 0){
+                System.out.printf("Okay %s, what is the title of the book? ★⋆.　\nType Here: ",userName);
+            }else if (i <= 2){
+                System.out.printf("What is the title, %s? ★⋆.　\nType Here: ",userName);
+            }else{
+                System.out.printf("You're a superstar reader %s! What title can I get you? ★⋆. ✦　\nType Here: ",userName);
+            }
+
+            String bookTitle = scanner.nextLine().trim().toLowerCase();
+            for (Book book:libraryInventory){
+                if (book.getTitle().toLowerCase().contains(bookTitle)){
+                    System.out.printf("%s by %s found and added!",book.getTitle(),book.getAuthor()); //I don't have time for another y or n to add but that would be ideal
+                    book.setCheckedOutTo(userName);
+                    book.setCheckedOut(true);
+                    cart[i]= book;
+                }
+            }
+            i++;
+            System.out.println("Would you like to check out another book?˚　.˚\n    [Y] Yes\n    [N] No\nType here: ");
+            String choiceCheck = scanner.nextLine().trim().toLowerCase();
+            if (choiceCheck.equals("n")){
+                choice= false;
             }
         }
-        i++;
-        return cart;
-    }
-    public static void displayCart(Book [] cart){
+        System.out.println("Here is what you have checked out:  ✦　　˚　　 . ★⋆.　　　.✦　　.　　.　　　✦　˚ 　　˚　.");
         for (Book book:cart){
-            System.out.printf("Here is what you have checked out:\n%d %s by %s | %s",book.getId(),book.getTitle(),book.getAuthor(),book.getIsbn());
+            if (book != null){
+                System.out.printf("%d %s by %s ISBN: %s\n",book.getId(),book.getTitle(),book.getAuthor(),book.getIsbn());
+            }
+
         }
     }
+    public static void showCheckedOut(Book[] libraryInventory){
+        int i = 1;
+        System.out.println("Currently Checked Out Books:  ✦　　˚　　 . ★⋆.　　　.✦　　.　　.　　　✦　˚ 　　˚　. ");
+        for (Book book:libraryInventory){
+            if (book.isCheckedOut()){
+                System.out.printf("Book %d: %d %s by %s checked out by %s\n",i,book.getId(),book.getTitle(),book.getAuthor(),book.getCheckedOutTo());
+                i++;
+            }
+        }
+
+    }
+    public static void checkInBook(Book[] libraryInventory,Scanner scanner){
+        boolean choice = true;
+        while (choice){
+
+            System.out.println("What is the ID of the book?");
+            int bookID = scanner.nextInt();
+            for (Book book:libraryInventory){
+                if (book.getId() == bookID){
+                    book.setCheckedOutTo("userName");
+                    book.setCheckedOut(false);
+                }
+            }
+
+            System.out.println("Book checked in! Would you like to check in another book?˚　.˚\n    [Y] Yes\n    [N] No\nType here: ");
+            String choiceCheck = scanner.nextLine().trim().toLowerCase();
+            if (choiceCheck.equals("n")){
+                System.out.println("Great, thank you for checking in! ˚　　 . ★⋆.　　　.✦　　.　");
+                choice = false;
+            }
+        }
+    }
+
 
 
 }
